b00a9f099e1d3cff7201def16c8bac8c3c7961ed
addictocrat <okandikkulak@gmail.com>
2025-11-25 14:44:08 +0300

    Updated readme

--- src/components/layout/search/Categories.tsx
+++ src/components/layout/search/Categories.tsx
@@ -1,9 +1,8 @@
 import configPromise from '@payload-config'
-import { getPayload } from 'payload'
 import clsx from 'clsx'
-import React, { Suspense } from 'react'
+import { getPayload } from 'payload'
+import { Suspense } from 'react'
 
-import { FilterList } from './filter'
 import { CategoryItem } from './Categories.client'
 
 async function CategoryList() {
@@ -16,7 +15,7 @@ async function CategoryList() {
 
   return (
     <div>
-      <h3 className="text-xs mb-2 text-neutral-500 dark:text-neutral-400">Category</h3>
+      <h3 className="text-xs mb-2 text-neutral-500 dark:text-neutral-400">Kategori</h3>
 
       <ul>
         {categories.docs.map((category) => {
